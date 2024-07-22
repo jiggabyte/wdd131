3ea68566244c698c86251e7231338bb7a505ce65
Jigga Byte <jigga.e10@gmail.com>
2024-07-22 13:08:42 +0100

    changed star emoji and color

--- scripts/form.js
+++ scripts/form.js
@@ -8,31 +8,22 @@ document.addEventListener('DOMContentLoaded', function () {
     ];
 
     const productSelect = document.getElementById('productName');
-    
+
     products.forEach(product => {
         const option = document.createElement('option');
         option.value = product.id;
         option.textContent = product.name;
         productSelect.appendChild(option);
     });
+
+    const yearSpan = document.getElementById("year");
+    const lastModifiedSpan = document.getElementById("last-modified");
+
+    // Set current year
+    const currentYear = new Date().getFullYear();
+    yearSpan.textContent = currentYear;
+
+    // Set last modified date
+    const lastModified = document.lastModified;
+    lastModifiedSpan.textContent = lastModified;
 });
-
-const yearSpan = document.getElementById("year");
-const lastModifiedSpan = document.getElementById("last-modified");
-
-// Set current year
-const currentYear = new Date().getFullYear();
-yearSpan.textContent = currentYear;
-
-// Set last modified date
-const lastModified = document.lastModified;
-lastModifiedSpan.textContent = lastModified;
-
-
-
-
-
-let reviewCount = localStorage.getItem('reviewCount') || 0;
-reviewCount++;
-localStorage.setItem('reviewCount', reviewCount);
-document.getElementById('reviewCount').textContent = reviewCount;
